d0cbc4e9724dcd4d64e68b95b5c9418de63574ec
Sven Brettschneider <brettschneider.sven@gmail.com>
2017-06-21 22:31:44 +0200

    Removing old spec files

--- src/app/game/games.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GamesPipe } from './games.pipe';
-
-describe('GamesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GamesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/game/tile/tile.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TileDirective } from './tile.directive';
-
-describe('TileDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TileDirective();
-    expect(directive).toBeTruthy();
-  });
-});
